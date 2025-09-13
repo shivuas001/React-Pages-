fd714806e6f1927024e31c50b130f50eaa9266ee
A SHIVA KUMAR <shivuas605@gmail.com>
2025-09-13 18:29:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {Router, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+
+
+const Router = createBrowserRouter(createRoutesFromElements())
+
+const App = ()=>{
+  return(
+    <>
+    <h1>hello world</h1>
+    </>
+  )
+}
+
+export default App
